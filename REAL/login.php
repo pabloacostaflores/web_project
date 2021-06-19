5bc8cfc55eba1428b0ae9fd3063f077988a43a6d
malsblack <47731706+malsblack@users.noreply.github.com>
2021-06-19 15:27:06 -0500

    Add files via upload
    anexo la vista del login

---
+++ REAL/login.php
@@ -0,0 +1,31 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "prueba";
+
+$conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+
+if (!$conn) 
+{
+    die("No hay conexion: ".mysqli_connect_error());
+}
+
+$usuario = $_POST["usuario"];
+$password = $_POST["pass"];
+$query = mysqli_query($conn,"SELECT * FROM admins WHERE Usuario = '".$usuario."' And Password = '".$password."'");
+$numero_fila = mysqli_num_rows($query);
+
+if ($numero_fila == 1){
+    header("Location: crud.html");
+    
+}
+else if ($numero_fila == 0){
+    echo "Tus credenciales son invalidas";
+}
+
+
+
+?>
