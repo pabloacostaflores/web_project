aa9ee0fb5e93b488c07859720d9acc372bed1902
PAF19 <hp.240.g3.pc@gmail.com>
2021-06-20 14:27:41 -0500

    Modified the sql queries from guardar_datos.php to the correct connection with the Data Base, but not tested yet

--- REAL/guardar_datos.php
+++ REAL/guardar_datos.php
@@ -1,5 +1,5 @@
-<?php 
-		
+<?php
+
 	$Num_Boleta = $_GET["numero_de_boleta"];
 	$nombre = $_GET["nombre"];
 	$ape_paterno = $_GET["apellido_paterno"];
@@ -19,15 +19,15 @@
 	$entidad =$_GET["entidad_pro"];
 	$promedio =$_GET["Promedio"];
 	$opcion =$_GET["Opcion"];
-	
-	
-	
-	$conexion = mysqli_connect("localhost","root","","Proyecto");
-	$sql_1= "(numBoleta, Nombre, ApePaterno, ApeMaterno, CURP, Genero,  FechaNacimiento, numBoleta, Estado, Delegacion, Calle, Colonia, CP, Telefono, Celular, Email, Escuela, Entidad, Promedio, Opcion)";
+
+
+
+	$conexion = mysqli_connect("localhost","root","","escom");
+	$sql_1= "(sudent_id, student_name, student_first_surname, student_second_surname, student_curp, student_gender, student_date_of_birth, student_state, student_municipality, student_street, student_neighborhood, student_postal_code, student_home_phone, student_phone_number, student_email, student_school_of_origin, student_state_of_origin, student_final_grades, student_escom_order)";
 	$sql_2= "('$Num_Boleta', '$nombre', '$ape_paterno', '$ape_materno', '$CURP', '$genero', '$fecha_nacimiento', '$estado', '$delegacion', '$calle', '$colonia', '$codigoP', '$tele_casa', '$num_telefono', '$correo', '$entidad', '$promedio', '$opcion')";
-	
-	$sql = "INSERT INTO alumno $sql_1 VALUES $sql_2" ;
-	
+
+	$sql = "INSERT INTO STUDENTS $sql_1 VALUES $sql_2" ;
+
 	mysqli_query($conexion, $sql);
-		
-?>	
+
+?>
