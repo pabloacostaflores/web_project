63bd696ee9ad71f2a28195f831bc2aac63e64ac5
PAF19 <hp.240.g3.pc@gmail.com>
2021-06-20 17:15:24 -0500

    modified guardar_datos.php and still not working.

--- guardar_datos.php
+++ guardar_datos.php
@@ -1,33 +1,38 @@
 <?php
+if($_GET){
+    if(isset($_GET['Enviar'])){
+        insert();
+			}
+}
+function insert(){
+	 $Num_Boleta = $_GET["numero_de_boleta"];
+	 $nombre = $_GET["nombre"];
+	 $ape_paterno = $_GET["apellido_paterno"];
+	 $ape_materno =$_GET["apellido_materno"];
+	 $CURP =$_GET["curp"];
+	 $genero =$_GET["genero"];
+	 $fecha_nacimiento =$_GET["bday"];
+	 $estado =$_GET["estado"];
+	 $delegacion =$_GET["delegacion"];
+	 $calle =$_GET["calle"];
+	 $colonia =$_GET["colonia"];
+	 $codigoP =$_GET["cp"];
+	 $tele_casa =$_GET["tel_casa"];
+	 $num_telefono =$_GET["celular"];
+	 $correo =$_GET["correo"];
+	 $escuela =$_GET["escuela"];
+	 $entidad =$_GET["entidad_pro"];
+	 $promedio =$_GET["promedio"];
+	 $opcion =$_GET["opcion"];
 
-	$Num_Boleta = $_GET["numero_de_boleta"];
-	$nombre = $_GET["nombre"];
-	$ape_paterno = $_GET["apellido_paterno"];
-	$ape_materno =$_GET["apellido_materno"];
-	$CURP =$_GET["curp"];
-	$genero =$_GET["genero"];
-	$fecha_nacimiento =$_GET["bday"];
-	$estado =$_GET["estado"];
-	$delegacion =$_GET["delegacion"];
-	$calle =$_GET["calle"];
-	$colonia =$_GET["colonia"];
-	$codigoP =$_GET["cp"];
-	$tele_casa =$_GET["tel_casa"];
-	$num_telefono =$_GET["celular"];
-	$correo =$_GET["correo"];
-	$escuela =$_GET["escuela"];
-	$entidad =$_GET["entidad_pro"];
-	$promedio =$_GET["promedio"];
-	$opcion =$_GET["opcion"];
 
 
+	 $conexion = mysqli_connect("localhost","root","","escom");
+	 $sql_1= "(sudent_id, student_name, student_first_surname, student_second_surname, student_curp, student_gender, student_date_of_birth, student_state, student_municipality, student_street, student_neighborhood, student_postal_code, student_home_phone, student_phone_number, student_email, student_school_of_origin, student_state_of_origin, student_final_grades, student_escom_order)";
+	 $sql_2= "('$Num_Boleta', '$nombre', '$ape_paterno', '$ape_materno', '$CURP', '$genero', '$fecha_nacimiento', '$estado', '$delegacion', '$calle', '$colonia', '$codigoP', '$tele_casa', '$num_telefono', '$correo', '$entidad', '$promedio', '$opcion')";
 
-	$conexion = mysqli_connect("localhost","root","","escom");
-	$sql_1= "(sudent_id, student_name, student_first_surname, student_second_surname, student_curp, student_gender, student_date_of_birth, student_state, student_municipality, student_street, student_neighborhood, student_postal_code, student_home_phone, student_phone_number, student_email, student_school_of_origin, student_state_of_origin, student_final_grades, student_escom_order)";
-	$sql_2= "('$Num_Boleta', '$nombre', '$ape_paterno', '$ape_materno', '$CURP', '$genero', '$fecha_nacimiento', '$estado', '$delegacion', '$calle', '$colonia', '$codigoP', '$tele_casa', '$num_telefono', '$correo', '$entidad', '$promedio', '$opcion')";
+	 $sql = "INSERT INTO STUDENTS $sql_1 VALUES $sql_2" ;
 
-	$sql = "INSERT INTO STUDENTS $sql_1 VALUES $sql_2" ;
-
-	mysqli_query($conexion, $sql);
-
+	 mysqli_query($conexion, $sql);
+}
 ?>
