267bc3efb2ac58abc6361aaf7e724f76dc3e89f6
Jose Juan Suarez Elizalde <jojusuarez46@gmail.com>
2021-06-22 01:53:10 -0500

    all validations has been done

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,4 +1,10 @@
-//Función para validar una CURP
+let vcurp = 1;
+let vbol = 1;
+let vcp = 1;
+let vtel = 1;
+let vcel = 1;
+let vemail = 1;
+
 function curpValida(curp) {
     var re = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/,
     
@@ -22,55 +28,146 @@ function curpValida(curp) {
         
     return true; //Validado
 }
-function validarEmail(valor) {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(valor)){
-        alert("La dirección de email " + valor + " es correcta.");
+//Función para validar una CURP
+function validarCurp(input){
+    var curp = input.value.toUpperCase();
+    // console.log(curp);
+    if (!curpValida(curp)) {
+        input.classList.add("wrong");
+        input.classList.remove("correct");
+        vcurp = 0;
     } else {
-        alert("La dirección de email es incorrecta.");
+        input.classList.remove("wrong");
+        input.classList.add("correct");
+        vcurp = 1;
     }
 }
 
-console.log(curpValida("SUEJ010405HDFRLNA0"));
+function boletaValida(boleta) {
+    var re = /^[P][PE]\d{8}$/,
+    validado = boleta.match(re);
+    if(validado)
+        return true;
+    var ri = /^\d{10}$/,
+    validado = boleta.match(ri);
+    if(validado)
+        return true;
+    return false; //Validado
+}
+//Función para validar una CURP
+function validarBoleta(input){
+    var boleta = input.value;
+    console.log(boleta);
+    if (!boletaValida(boleta)) {
+        input.classList.add("wrong");
+        input.classList.remove("correct");
+        vbol = 0;
+    } else {
+        input.classList.remove("wrong");
+        input.classList.add("correct");
+        vbol = 1;
+    }
+}
+
+function cvalida(input) {
+    var re = /^\d{5}$/,
+    validation = input.match(re); 
+    if(validation)return true;
+    return false;
+}
+//Función para validar una CURP
+function validarCP(input){
+    var CP = input.value;
+    if (!cvalida(CP)) {
+        input.classList.add("wrong");
+        input.classList.remove("correct");
+        vcp = 0;
+    } else {
+        input.classList.remove("wrong");
+        input.classList.add("correct");
+        vcp = 1;
+    }
+}
+function telvalida(input) {
+    var re = /^\d{10}$/,
+    validation = input.match(re); 
+    if(validation)return true;
+    return false;
+}
+//Función para validar una CURP
+function validarTel(input){
+    var tel = input.value;
+    if (!telvalida(tel)) {
+        input.classList.add("wrong");
+        input.classList.remove("correct");
+        vtel = 0;
+    } else {
+        input.classList.remove("wrong");
+        input.classList.add("correct");
+        vtel = 1;
+    }
+}
+function celvalida(input) {
+    var re = /^\d{10}$/,
+    validation = input.match(re); 
+    if(validation)return true;
+    return false;
+}
+//Función para validar una CURP
+function validarCel(input){
+    var Cel = input.value;
+    if (!celvalida(Cel)) {
+        input.classList.add("wrong");
+        input.classList.remove("correct");
+        vcel = 0;
+    } else {
+        input.classList.remove("wrong");
+        input.classList.add("correct");
+        vcel = 1;
+    }
+}
+function emailValida(input) {
+    var re = /^\d{10}$/,
+    validation = input.match(re); 
+    if(validation)return true;
+    return false;
+}
+function emailValidar(valor) {
+    var punto = false;
+    var str = valor;
+    console.log(str);
+    for(var i=0;i<str.length;i++){
+        if(str.charAt(i) == '.')punto=true;
+    }
+    if(/^[a-z0-9!#$%&'+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'+/=?^_`{|}~-]+)@(?:[a-z0-9](?:[a-z0-9-][a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(valor) && punto){
+        return true;
+    } return false;
+}
+//Función para validar una CURP
+function validarEmail(input){
+    var Cel = input.value;
+    if (!emailValidar(Cel)) {
+        input.classList.add("wrong");
+        input.classList.remove("correct");
+        vemail = 0;
+    } else {
+        input.classList.remove("wrong");
+        input.classList.add("correct");
+        vemail = 1;
+    }
+}   
 
 function f(){
     location.reload();
 }
 function en(){
-    // para select no clase sino id
-    // alert("Tus datos son correctos?");
-    var obj = new Object();
-    obj.bol = document.querySelector(".js-boleta").value;
-    obj.nombre = document.querySelector(".js-nombre").value;
-    obj.apPaterno = document.querySelector(".js-apPaterno").value;
-    obj.apMaterno = document.querySelector(".js-apMaterno").value;
-    obj.curp = document.querySelector(".js-curp").value;
-    console.log(obj.curp);
-    if(!curpValida(toString(obj.curp))){
-        document.querySelector(".js-curp").classList.add("wrong");
-        return;
+    console.log({vcurp ,vbol,vcp,vtel,vcel,vemail });
+    if(vcurp == 1 && vbol == 1 && vcp == 1 && vtel == 1 && vcel == 1 && vemail == 1){
+        alert("quedas registrado xd");
+        document.querySelector(".js-submit").click();
     }else{
-        document.querySelector(".js-curp").classList.add("correct");
+        alert("huevos");
     }
-    obj.genero = document.querySelector("#js-genero").innerHTML;
-    obj.fecha = document.querySelector(".js-fecha").value;
-    obj.estado = document.querySelector("#js-estado").innerHTML;
-    obj.del = document.querySelector(".js-del").value;
-    obj.col = document.querySelector(".js-col").value;
-    obj.dir = document.querySelector(".js-dir").value;
-    obj.cp = document.querySelector(".js-cp").value;
-    obj.tel = document.querySelector(".js-tel").value;
-    obj.cel = document.querySelector(".js-cel").value;
-    obj.email = document.querySelector(".js-email").value;
-    obj.estpro = document.querySelector("#js-escuela").innerHTML;
-    obj.pro = document.querySelector("#js-entidad_pro").innerHTML;
-    obj.opci = document.querySelector("#js-opcion").innerHTML;
-    console.log(obj);
-    //insertar lo de arriba en php
-
-    // if(exito)alert("Datos enviados con exito!");
-    // else alert("Hubo un error en el sistema.");
-    alert("Te hemos enviado a tu correo el pdf de tu inscripcion");
-
 }
 function st(){
     var x = document.querySelector(".js-lstudent");
